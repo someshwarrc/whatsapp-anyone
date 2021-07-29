f8a86052729e5e5c42e2fb5dcf6024dafb3ba693
someshwarrc <someshwarrc@gmail.com>
2021-07-29 23:31:18 +0530

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import codes from "./CountryCodes.js";
+import React from "react";
 
 function App() {
+  const names = Object.keys(codes);
+  const [name, setName] = React.useState(names[0]);
+  const [phone, setPhone] = React.useState("");
+  const redirect = (e, extension, number) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const baseUrl = "http://wa.me/";
+
+    if (number.length !== 0)
+      window.location.href = baseUrl + "+" + extension + number;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={"container"}>
+      <div>
+        Fed up with <span className="decorate">saving</span> the phone number
+        everytime you want to WhatsApp someone?
+      </div>
+      <p>
+        Well you <span className="decorate">don't</span> need to save anymore!
+      </p>
+      <form
+        onSubmit={(e) => redirect(e, codes[name], phone)}
+        className={"form-wrapper"}
+      >
+        <label htmlFor="country">Country</label>
+        <select
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className={"name"}
+          id="country"
         >
-          Learn React
-        </a>
-      </header>
+          {names.map((name) => (
+            <option value={name} key={name + `${Math.random() * 1000}`}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <div className={"code-number"}>
+          <input
+            type="text"
+            name="extension"
+            value={`+${codes[name]}`}
+            id="ext"
+            disabled
+          />
+          <input
+            type="text"
+            name="phone_number"
+            id="number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="Enter number without extension.."
+          />
+        </div>
+        <button>WHATSAPP NOW</button>
+      </form>
     </div>
   );
 }
